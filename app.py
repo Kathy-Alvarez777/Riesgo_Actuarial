b5bf710701e179a2f8852c26bc37f2740dee5a43
Kathy-Alvarez777 <kathalvarez0493@gmail.com>
2026-05-13 22:03:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from groq import Groq
 
 
 st.set_page_config(page_title="Riesgo actuarial", layout="centered")
-st.title("Predicción de riesgo actuarial")
+st.title("Predicción de riesgo actuarial_Kathy_Alvare_M")
 
 
 @st.cache_resource
